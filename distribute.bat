d18e4475d221772e7cc9a9c06d1b54f55b1a2c8a
Abathur8bit <Abathur8bit@8bitcoder.com>
2025-03-28 09:38:52 -0500

    - Fixed distribute so it uses a build directory and copies over global lib files

--- distribute.bat
+++ distribute.bat
@@ -1,36 +1,62 @@
 @echo off
 @if "%1" == "" goto usage
 
-set VERSION="%1"
-set GAME_BASE="badwizard"
+set VERSION=%1
+set GAME_BASE=badwizard
 set LOVE="C:\util\love2d\love-11.5-win64"
-set GAME="%GAME_BASE%-%1"
-set DIST="dist"
+set GAME=%GAME_BASE%-%VERSION%
+set DIST=dist
+set BUILD=%DIST%\build
 
-rm -f  %GAME%-web.love
-rm -f  %GAME%.love
+rm -f  %DIST%\%GAME%-web.love
+rm -f  %DIST%\%GAME%.love
+rm -f  %DIST%\%GAME%.zip
+mkdir %BUILD%\assets
+mkdir %BUILD%\lib
+mkdir %BUILD%\maps
+del /q %BUILD%\assets\*
+del /q %BUILD%\lib\*
+del /q %BUILD%\maps\*
+del /q %BUILD%\*
+
+echo Build web package
 set INBROWSER=true
 set RESOLUTION=1000
 set FULLSCREEN=false
 set LOVE_GAME=%GAME%-web.love
-call :build_love
+call :build_conf
+xcopy /y *.lua %BUILD%
+xcopy /y README.txt %BUILD%
+xcopy /y /s lib %BUILD%\lib
+xcopy /y ..\lib %BUILD%\lib
+xcopy /y assets\*.png %BUILD%\assets
+xcopy /y maps\*.png %BUILD%\maps
+xcopy /y maps\*.lua %BUILD%\maps
+cd %BUILD%
 zip -9 -r %LOVE_GAME% *.lua assets\*.png README.txt lib\* maps\*.png maps\*.lua 
+move *.love ..
+cd ..\..
 
+echo Build love package
 set INBROWSER=false
-set RESOLUTION=1600
+set RESOLUTION=1920
 set FULLSCREEN=true
 set LOVE_GAME=%GAME%.love
-call :build_love
+call :build_conf
+xcopy /y *.lua %BUILD%
+xcopy /y README.txt %BUILD%
+xcopy /y /s lib %BUILD%\lib
+xcopy /y /s ..\lib %BUILD%\lib
+xcopy /y assets %BUILD%\assets
+xcopy /y maps\*.png %BUILD%\maps
+xcopy /y maps\*.lua %BUILD%\maps
+cd %BUILD%
 zip -9 -r %LOVE_GAME% *.lua assets\* README.txt lib\* maps\*.png maps\*.lua 
+move *.love ..
+cd ..\..
 
-:build_zip
-mkdir %DIST%
-del /q %DIST%\*.zip
-del /q %DIST%\*.love
-move *.love %DIST%
-
+echo Build Windows EXE
 cd %DIST%
-
 mkdir %GAME%
 cd %GAME%
 copy /b %LOVE%\love.exe+..\%GAME%.love %GAME_BASE%.exe
@@ -53,15 +79,15 @@ echo Example: distribute 1.0
 :end
 exit /b
 
-:build_love
-echo buildVersion=%VERSION%>conf.lua
-echo inbrowser=%INBROWSER%>>conf.lua
+:build_conf
+echo buildVersion=%VERSION%>  conf.lua
+echo inbrowser=%INBROWSER%>>  conf.lua
 echo resolution=%RESOLUTION%>>conf.lua
 echo fullscreen=%FULLSCREEN%>>conf.lua
-echo release=true>>conf.lua
-echo function love.conf(t)>>conf.lua
-echo  t.console=true>>conf.lua
-echo end>>conf.lua
+echo release=true>>           conf.lua
+echo function love.conf(t)>>  conf.lua
+echo   t.console=true>>       conf.lua
+echo end>>                    conf.lua
 exit /b
 
 
---
+++ lib/axmath.lua
@@ -0,0 +1,56 @@
+local axmath={}
+
+function axmath.radiansTable(step)
+  local radians={}
+  local index=0
+  for degrees=0,359,step do
+    local angle=degrees*math.pi/180
+    radians[index]=angle
+    print("degree,radian",degrees,angle)
+    index=index+1
+  end
+  return radians
+end
+
+--[[
+Create a table so you can lookup a radian based on
+a degree. Table starts at 0, ends at 359.
+local degree=5
+local radTable=axmath.degree2radianTable(step)
+local rad=radTable[degree]
+--rad=0.087266462599716
+]]--
+function axmath.degree2radianTable(step)
+  local radians={}
+  for degrees=0,359,step do
+    local angle=degrees*math.pi/180
+    radians[degrees]=angle
+  end
+  return radians
+end
+
+function axmath.degreeTable(step)
+  local degrees={}
+  local deg=0
+  local index=0
+  local counter=0
+  for d=0,359 do
+    degrees[index]=deg
+    index=index+1
+    counter=counter+1
+    if counter>=step then
+      counter=0
+      deg=deg+step
+    end
+  end
+  return degrees
+end
+
+-- degreeTable should have values for 0-359
+function axmath.snapDegree(degree,degreeTable)
+  local d=math.floor(degree)%360 --make sure we are 0-359
+  assert(d>=0 and d<=359,"degree must be 0-359")
+  return degreeTable[d]
+end
+
+return axmath
